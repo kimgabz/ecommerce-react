4915657fa516eefde2725e67c9921df9d483b5a1
kim <khemgabz@gmail.com>
2021-02-02 10:37:51 +0800

    order

--- src/pages/admin/admin.dashboard.jsx
+++ src/pages/admin/admin.dashboard.jsx
@@ -1,7 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
 import AdminNav from "../../components/navigation/admin.nav";
+import { getOrders, changeStatus } from "../../functions/admin.functions";
+import Orders from "../../components/order/Orders";
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  const loadOrders = () =>
+    getOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then((res) => {
+      toast.success("Status updated");
+      loadOrders();
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -11,6 +36,8 @@ const AdminDashboard = () => {
 
         <div className="col">
           <h4>Admin Dashboard</h4>
+          {/* {JSON.stringify(orders)} */}
+          <Orders orders={orders} handleStatusChange={handleStatusChange} />
         </div>
       </div>
     </div>
